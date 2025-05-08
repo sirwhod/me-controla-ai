3b55ce06d8e57aaa8a9bd8f0d324fecf86e0ca4b
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-05-08 20:37:03 -0300

    feat: configura next-auth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    firebaseIdToken?: string;
+  }
+  interface User {
+    firebaseIdToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    firebaseIdToken?: string;
+  }
+}
