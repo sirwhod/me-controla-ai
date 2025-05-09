b2657fda011a5838eca2eae8d10c980243b7b5a4
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-05-08 22:12:33 -0300

    feat: Configura login na plataforma

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { PiggyBank } from "lucide-react"
+
+export default function DashboardPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <PiggyBank className="size-4" />
+          </div>
+          Poupa Grana.
+        </a>
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  )
+}
