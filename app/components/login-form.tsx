711ac4122118460252db63dc981c0702166f8028
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-05-10 00:29:57 -0300

    refactor: traduz footer do logi-form

--- app/components/login-form.tsx
+++ app/components/login-form.tsx
@@ -27,11 +27,11 @@ export function LoginForm({
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Bem-Vindo(a).</CardTitle>
           <CardDescription>
-            Login with your Google account
+            Entrar com sua conta do Google.
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
                 <Button variant="outline" onClick={handleGoogleSignIn} className="w-full">
@@ -49,45 +49,40 @@ export function LoginForm({
                   Ou continue com
                 </span>
               </div>
-              <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Senha</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Esqueceu a senha?
-                    </a>
+              <form>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
                   </div>
-                  <Input id="password" type="password" required />
+                  <div className="grid gap-3">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Senha</Label>
+                      <a
+                        href="#"
+                        className="ml-auto text-sm underline-offset-4 hover:underline"
+                      >
+                        Esqueceu a senha?
+                      </a>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Entrar
+                  </Button>
                 </div>
-                <Button type="submit" className="w-full">
-                  Entrar
-                </Button>
-              </div>
-              <div className="text-center text-sm">
-                Não tem uma conta?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Cadastre-se
-                </a>
-              </div>
+              </form>
             </div>
-          </form>
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        Ao Clicar em entrar, você está aceitando os <a href="#">Termos de Serviço</a>{" "}
+        e a <a href="#">Politica de Privacidade</a>.
       </div>
     </div>
   )
