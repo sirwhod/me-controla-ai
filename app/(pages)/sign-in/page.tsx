fffaa99dc2808600f8f39a5d05a7de20cdccbe22
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-05-13 19:58:24 -0300

    style: ajusta tela de login

--- app/(pages)/sign-in/page.tsx
+++ app/(pages)/sign-in/page.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react"
 import { redirect } from "next/navigation"
 import Image from "next/image"
 
+import Placeholder from "@/public/placeholder.svg"
 import Logo from "@/public/logo.svg"
 
 export default function LoginPage() {
@@ -17,14 +18,29 @@ export default function LoginPage() {
     }
   }, [status])
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <Image src={Logo} alt="" width={24} height={24} />
+    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="flex flex-col gap-4 p-6 md:p-10">
+      <div className="flex justify-center gap-2 md:justify-start">
+        <a href="#" className="flex items-center gap-2 font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Image src={Logo} alt="" width={24} height={24} />
+          </div>
           MeControla.AI
         </a>
-        <LoginForm />
+      </div>
+      <div className="flex flex-1 items-center justify-center">
+        <div className="w-full max-w-xs">
+          <LoginForm />
+        </div>
       </div>
     </div>
+    <div className="relative hidden bg-muted lg:block">
+      <Image
+        src={Placeholder}
+        alt="Image"
+        className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+      />
+    </div>
+  </div>
   )
 }
