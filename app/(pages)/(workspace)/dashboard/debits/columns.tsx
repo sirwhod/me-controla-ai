eca3197653720b58433631fe6c1a1f6dc6bb106b
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-09-03 22:21:55 -0300

    feat: ajusta responsividade em dispositivos mobile

--- app/(pages)/(workspace)/dashboard/debits/columns.tsx
+++ app/(pages)/(workspace)/dashboard/debits/columns.tsx
@@ -44,7 +44,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <BanknoteArrowDown className="w-4 h-4"/>
+                  <BanknoteArrowDown className="w-6 md:w-4 h-6 md:h-4"/>
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{type}</strong>
@@ -58,7 +58,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <Pin className="w-4 h-4" />
+                  <Pin className="w-6 md:w-4 h-6 md:h-4" />
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{type}</strong>
@@ -72,7 +72,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <CreditCard className="w-4 h-4" />
+                  <CreditCard className="w-6 md:w-4 h-6 md:h-4" />
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{type}</strong>
@@ -86,7 +86,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <CalendarSync className="w-4 h-4"/>
+                  <CalendarSync className="w-6 md:w-4 h-6 md:h-4"/>
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{type}</strong>
@@ -123,7 +123,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <Landmark className="w-4 h-4"/>
+                  <Landmark className="w-6 md:w-4 h-6 md:h-4"/>
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{paymentMethod}</strong>
@@ -138,7 +138,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <CreditCard className="w-4 h-4"/>
+                  <CreditCard className="w-6 md:w-4 h-6 md:h-4"/>
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{paymentMethod}</strong>
@@ -153,7 +153,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                   <Banknote className="w-4 h-4"/>
+                   <Banknote className="w-6 md:w-4 h-6 md:h-4"/>
                 </TooltipTrigger>
                 <TooltipContent>
                   <strong>{paymentMethod}</strong>
@@ -168,7 +168,7 @@ export const columns: ColumnDef<Debit>[] = [
             <div className="flex flex-row gap-2 items-center">
               <Tooltip>
                 <TooltipTrigger>
-                  <svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" className="mb-3 w-4 h-4">
+                  <svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" className="mb-3 w-6 md:w-4 h-6 md:h-4">
                     <path d="M11.917 11.71a2.046 2.046 0 0 1-1.454-.602l-2.1-2.1a.4.4 0 0 0-.551 0l-2.108 2.108a2.044 2.044 0 0 1-1.454.602h-.414l2.66 2.66c.83.83 2.177.83 3.007 0l2.667-2.668h-.253zM4.25 4.282c.55 0 1.066.214 1.454.602l2.108 2.108a.39.39 0 0 0 .552 0l2.1-2.1a2.044 2.044 0 0 1 1.453-.602h.253L9.503 1.623a2.127 2.127 0 0 0-3.007 0l-2.66 2.66h.414z"/>
                     <path d="m14.377 6.496-1.612-1.612a.307.307 0 0 1-.114.023h-.733c-.379 0-.75.154-1.017.422l-2.1 2.1a1.005 1.005 0 0 1-1.425 0L5.268 5.32a1.448 1.448 0 0 0-1.018-.422h-.9a.306.306 0 0 1-.109-.021L1.623 6.496c-.83.83-.83 2.177 0 3.008l1.618 1.618a.305.305 0 0 1 .108-.022h.901c.38 0 .75-.153 1.018-.421L7.375 8.57a1.034 1.034 0 0 1 1.426 0l2.1 2.1c.267.268.638.421 1.017.421h.733c.04 0 .079.01.114.024l1.612-1.612c.83-.83.83-2.178 0-3.008z"/>
                   </svg>
